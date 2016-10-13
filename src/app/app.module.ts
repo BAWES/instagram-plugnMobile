f61e7e0929343b44b4488ee84d0c5031ad8d1c39
Khalid <khalid@bawes.net>
2016-10-13 11:25:36 +0300

    Refactor module configuration files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,29 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 // Providers / Services
 import { AuthService } from '../providers/auth.service'
 
-@NgModule({
-  declarations: [
+export const pages = [
     MyApp,
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
-  ],
+    TabsPage,
+    LoginPage
+  ];
+
+@NgModule({
+  declarations: pages,
+  entryComponents: pages,
   imports: [
     IonicModule.forRoot(MyApp)
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
   providers: [
     AuthService
-  ]
+  ],
+  bootstrap: [IonicApp]
 })
 export class AppModule {}
