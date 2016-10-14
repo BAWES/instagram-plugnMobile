2fced36347d283e66ff2a3dd2b774cd71c4731fd
Khalid <khalid@bawes.net>
2016-10-14 17:23:43 +0300

    Initial work on rxjs oauth implementation

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,10 @@ import { NavController, Platform } from 'ionic-angular';
 
 import {InAppBrowser} from 'ionic-native';
 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/first';
+
 import { TabsPage } from '../tabs/tabs';
 
 /*
@@ -14,7 +18,9 @@ import { TabsPage } from '../tabs/tabs';
 })
 export class LoginPage {
 
-  private browser;
+  private browser: InAppBrowser;
+  private browserLoadEvents;
+  private browserCloseEvents;
 
   constructor(public navCtrl: NavController, private platform: Platform) {}
 
@@ -30,7 +36,25 @@ export class LoginPage {
   loadUrl(url: string){
     this.platform.ready().then(() => {
         this.browser = new InAppBrowser(url, "_self", "location=yes,zoom=no");
+
+        // Keep track of urls loaded
+        this.browserLoadEvents = this.browser.on("loadstart");
+        this.browserLoadEvents = this.browserLoadEvents.map(res => res.url).subscribe(url => {
+          this.doActionBasedOnUrl(url);
+        });
+
+        // Keep track of browser if closed
+        this.browserCloseEvents = this.browser.on("exit").first().subscribe(resp => {
+          // Browser closed, unsubscribe from previous observables
+          this.browserLoadEvents.unsubscribe();
+          this.browserCloseEvents.unsubscribe();
+          console.log("Window closed, unsubscribed from observables");
+        });
     });
+  }
+
+  doActionBasedOnUrl(url){
+    console.log("Doing stuff on url", url);
   }
 
   loadSignupPage(){
