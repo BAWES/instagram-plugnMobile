8169b2b690979e77f7a437f7cee154fdb27c8935
Khalid <khalid@bawes.net>
2016-11-06 17:21:17 +0300

    Made menus persist across pages

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AboutPage } from '../about/about';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -10,6 +12,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  loadAboutPage(){
+    this.navCtrl.push(AboutPage);
   }
 
 }
