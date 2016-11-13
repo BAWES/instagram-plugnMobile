1314ae4935f20f9824aa3ca20d8109c4de47aa68
Khalid <khalid@bawes.net>
2016-11-13 20:41:53 +0300

    Hide tabs when keyboard is open

--- src/providers/keyboard.service.ts
+++ src/providers/keyboard.service.ts
@@ -28,6 +28,7 @@ export class KeyboardService {
     Keyboard.onKeyboardShow().subscribe(e => {
       this.isKeyboardOpen = true;
       this.keyboardHeight = e.keyboardHeight;
+      document.body.classList.add('keyboard-is-open');
 
       // Force Change Detection in Angular
       this.ref.tick();
@@ -36,6 +37,7 @@ export class KeyboardService {
     // Subscribe to Keyboard Closed Events
     Keyboard.onKeyboardHide().subscribe(e => {
       this.isKeyboardOpen = false;
+      document.body.classList.remove('keyboard-is-open');
 
       // Force Change Detection in Angular
       this.ref.tick();
