cfedc5d0a288b6201ba0a070f697c951f4d866b7
Khalid <khalid@bawes.net>
2016-11-13 20:46:28 +0300

    Fix keyboard service inclusion

--- src/pages/account/account-tabs/account-tabs.ts
+++ src/pages/account/account-tabs/account-tabs.ts
@@ -5,6 +5,8 @@ import { NavController } from 'ionic-angular';
 import { MediaPage } from '../media/media';
 import { ConversationPage } from '../conversation/conversation';
 
+import { KeyboardService } from '../../../providers/keyboard.service';
+
 /*
   AccountTabs tabs.
 */
@@ -17,8 +19,9 @@ export class AccountTabsPage {
   tab1Root: any = ConversationPage;
   tab2Root: any = MediaPage;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public keyboard: KeyboardService) {
+    // Keyboard service is required in constructor to hide the tabs.
+    // If user was already logged in, a singleton wouldn't have been created from login page
   }
 
 }
