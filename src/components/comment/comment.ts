dab289ed0de04d7e22735bf6877807b24590c06f
Khalid <khalid@bawes.net>
2016-11-19 17:48:26 +0300

    Setup copy to clipboard and additional guidance

--- src/components/comment/comment.ts
+++ src/components/comment/comment.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Platform, Haptic, ToastController } from 'ionic-angular';
+import { Clipboard } from 'ionic-native';
 
 // Models
 import { Comment } from '../../models/comment';
@@ -16,7 +18,42 @@ export class CommentComponent {
 
   @Input('value') comment: Comment;
 
-  constructor(public accounts: AccountService) {
+  constructor(
+    public accounts: AccountService,
+    private _platform: Platform,
+    private _haptic: Haptic,
+    private _toastCtrl: ToastController
+    ) {
   }
 
+  /**
+   * Present toast showing available options when dealing with this item
+   */
+  notifyOptions(){
+
+    // Add link to Media view on swipe
+    let toast = this._toastCtrl.create({
+        message: 'Swipe the comment left for additional options',
+        position: 'bottom',
+        duration: 1500
+      });
+      toast.present();
+  }
+
+  /**
+   * Copy comment content to device clipboard
+   */
+  copyToClipboard(){
+    if (this._platform.is('cordova')) {
+      Clipboard.copy(`@${this.comment.comment_by_username}: ${this.comment.comment_text}`);
+      this._haptic.notification({type: 'success'});
+      let toast = this._toastCtrl.create({
+        message: 'Comment has been copied to the clipboard',
+        position: 'top',
+        duration: 1500
+      });
+      toast.present();
+    }
+  }
+    
 }
