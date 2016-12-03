2faa6e1a02e3c67a6d9b4d3690698c6c449ccb1f
Khalid <khalid@bawes.net>
2016-12-03 19:41:39 +0300

    Account activity display functional

--- src/pages/statistics/agent-activity/agent-activity.ts
+++ src/pages/statistics/agent-activity/agent-activity.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 
 import { AccountService } from '../../../providers/logged-in/account.service';
+import { ActivityService } from '../../../providers/logged-in/activity.service';
 
 /*
   Agent Activity Statistics Page
@@ -12,18 +13,36 @@ import { AccountService } from '../../../providers/logged-in/account.service';
 })
 export class AgentActivityPage {
 
+  public isLoading = false;
+  public accountActivity; // Loaded activity data
+
   constructor(
     public navCtrl: NavController,
     public accounts: AccountService,
+    public activityService: ActivityService,
     ) {}
 
   /**
    * On Page Enter
    */
   ionViewDidEnter() {
-
+    // Load and populate media detail
+    this._loadAgentActivity();
   }
 
+
+  /**
+   * Load Agent Activity for the currently active account
+   */
+  private _loadAgentActivity(){
+    this.isLoading = true;
+    this.activityService
+        .getActivityOnAccount(this.accounts.activeAccount.user_id)
+        .subscribe(jsonResponse => {
+          this.isLoading = false;
+          this.accountActivity = jsonResponse;
+        });
+  }
   
 
 }
