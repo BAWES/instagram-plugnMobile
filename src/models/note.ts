a661dc007385fb673562e663692b47d25fc08ada
Khalid <khalid@bawes.net>
2016-12-05 18:49:02 +0300

    Setup the note model for future use

---
+++ src/models/note.ts
@@ -0,0 +1,10 @@
+
+export interface Note{
+    id: number;
+    title: number;
+    content: number;
+    created_by: number;
+    updated_by: string;
+    created_datetime: string;
+    updated_datetime: string;
+}
