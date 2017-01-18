67c7702ff489f5426479ae4328a11236e45f59ff
Khalid <khalid@bawes.net>
2017-01-19 00:07:48 +0300

    Refactor slide logic to html

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -5,43 +5,17 @@ import { MenuController, NavController } from 'ionic-angular';
 import { LoginPage } from '../start-pages/login/login';
 
 
-export interface Slide {
-  title: string;
-  description: string;
-  image: string;
-}
-
 @Component({
   selector: 'page-tutorial',
   templateUrl: 'tutorial.html'
 })
 export class TutorialPage {
-  slides: Slide[];
   showSkip = true;
 
   constructor(
     public navCtrl: NavController, 
     public menu: MenuController
-    ) {
-
-    this.slides = [
-        {
-          title: "Slide1 - title",
-          description: "desc1",
-          image: 'assets/img/ica-slidebox-img-1.png',
-        },
-        {
-          title: "Slide2 - title",
-          description: "desc2",
-          image: 'assets/img/ica-slidebox-img-2.png',
-        },
-        {
-          title: "Slide3 - title",
-          description: "desc3",
-          image: 'assets/img/ica-slidebox-img-3.png',
-        }
-      ];
-  }
+    ) {}
 
   ionViewDidEnter() {
     // the root left menu should be disabled on the tutorial page
